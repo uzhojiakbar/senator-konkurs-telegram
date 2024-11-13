d3bbecf018c385f58080ffad162be9fdf2ecf545
uzhojiakbar <uzhojiakbar3@gmail.com>
2024-11-13 20:53:50 +0500

    Button changed

--- mocks/mockChannels.js
+++ mocks/mockChannels.js
@@ -1,6 +1,6 @@
 module.exports = [
-  { name: "➕ Qo'shilish", username: "@FALCON_ARENDA" },
-  { name: "➕ Qo'shilish", username: "@FALCONACCSAVDO" },
-  { name: "➕ Qo'shilish", username: "@SENATOR_PUBGM" },
-  { name: "➕ Qo'shilish", username: "@asicoaccsavdo" },
+  { name: "Qo'shilish", username: "@FALCON_ARENDA" },
+  { name: "Qo'shilish", username: "@FALCONACCSAVDO" },
+  { name: "Qo'shilish", username: "@SENATOR_PUBGM" },
+  { name: "Qo'shilish", username: "@asicoaccsavdo" },
 ];
