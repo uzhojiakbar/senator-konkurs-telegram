30ebf28ac2045d5d2637456dfdd2472bb0cad253
uzhojiakbar <uzhojiakbar3@gmail.com>
2024-11-14 21:51:28 +0500

    changed

--- mocks/mockChannels.js
+++ mocks/mockChannels.js
@@ -1,6 +1,9 @@
-module.exports = [
-  { name: "Qo'shilish", username: "@FALCON_ARENDA" },
-  { name: "Qo'shilish", username: "@FALCONACCSAVDO" },
-  { name: "Qo'shilish", username: "@SENATOR_PUBGM" },
-  { name: "Qo'shilish", username: "@asicoaccsavdo" },
-];
+module.exports = [{ name: "Qo'shilish", username: "@murodillayev_hojiakbar" }];
+
+// module.exports = [
+//   { name: "Qo'shilish", username: "@FALCON_ARENDA" },
+//   { name: "Qo'shilish", username: "@SENATORAZART" },
+//   { name: "Qo'shilish", username: "@FALCONACCSAVDO" },
+//   { name: "Qo'shilish", username: "@SENATOR_PUBGM" },
+//   { name: "Qo'shilish", username: "@asicoaccsavdo" },
+// ];
